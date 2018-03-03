4e0a2cb932a4847427caa1cb2fdafbf4eb2df90b
jonathon reich <jonathonreich@MacBook-Air.local>
2018-03-03 10:11:36 -0500

    finshed2

--- models/index.js
+++ models/index.js
@@ -13,7 +13,8 @@ if(process.env.JAWSDB_URL)
 else{
   env = "development"
 }
-
+var config = require('../config/config.json')[env];
+console.log(config);
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
